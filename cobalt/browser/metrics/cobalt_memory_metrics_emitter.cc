03a95777a184eafa8faea942b848e47c464b6f8e
Ahmed Elzeiny <ahmedelzeiny@google.com>
2026-07-19 13:04:35 -0700

    Save local changes for UMA on RDK project.
    TAG=agy
    CONV=dba4b09d-7224-498f-a7b2-387803d3f27d

--- cobalt/browser/metrics/cobalt_memory_metrics_emitter.cc
+++ cobalt/browser/metrics/cobalt_memory_metrics_emitter.cc
@@ -293,6 +293,7 @@ void CobaltMemoryMetricsEmitter::FetchAndEmitProcessMemoryMetrics() {
     }
     instrumentation->RequestGlobalDump(mad_list, std::move(callback));
   } else {
+    LOG(WARNING) << "MemoryInstrumentation instance is null.";
     base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE,
         base::BindOnce(&CobaltMemoryMetricsEmitter::ReceivedMemoryDump, this,
@@ -309,6 +310,8 @@ void CobaltMemoryMetricsEmitter::ReceivedMemoryDump(
 
   memory_dump_in_progress_ = false;
   if (!success || !dump) {
+    LOG(WARNING) << "ReceivedMemoryDump failed. success=" << success
+                 << ", dump=" << (dump ? "not null" : "null");
     if (callback_for_testing_) {
       std::move(callback_for_testing_).Run();
     }
